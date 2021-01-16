26aabd3baf2edea001b6066b8d6de557faa2ef81
bazunaka-macbook <bazunaka32@gmail.com>
2021-01-16 16:46:55 +0300

    add sqlalchemy. create flask_db.

--- hello.py
+++ hello.py
@@ -3,6 +3,10 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class NameForm(Form):
    name = StringField('Ваше имя?', validators=[Required()])
@@ -10,7 +14,27 @@ class NameForm(Form):
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'flask.db')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+class Role (db.Model):
+   __tablename__ = 'roles'
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(64), unique=True)
+
+   def __repr__ (self):
+      return '<Role %r>' % self.name
+
+class User (db.Model):
+   __tablename__ = 'users'
+   id = db.Column(db.Integer, primary_key=True)
+   username = db.Column(db.String(64), unique=True, index=True)
+
+   def __repr__ (self):
+      return '<User %r>' % self.username
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
