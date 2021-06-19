049a942a956f3f7f913cf442ac38cdac0b2bb8ff
bazunaka-macbook <bazunaka32@gmail.com>
2021-06-20 00:01:13 +0300

    config for mac

--- hello.py
+++ hello.py
@@ -5,15 +5,17 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Shell, Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 from flask_mail import Mail
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class NameForm(Form):
-   name = StringField('Ваше имя?', validators=[Required()])
-   submit = SubmitField('Подтвердить')
+    name = StringField('Ваше имя?', validators=[Required()])
+    submit = SubmitField('Подтвердить')
+
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -24,60 +26,70 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
+
 def make_shell_context():
-   return dict(app = app, db = db, User = User, Role = Role)
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
+#migrate = Migrate(app, db)
+#manager.add_command('db', MigrateCommand)
 
 mail = Mail(app)
 
+
 class Role (db.Model):
-   __tablename__ = 'roles'
-   id = db.Column(db.Integer, primary_key=True)
-   name = db.Column(db.String(64), unique=True)
-   users = db.relationship('User', backref = 'role')
-   
-   def __repr__ (self):
-      return '<Role %r>' % self.name
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
 
 class User (db.Model):
-   __tablename__ = 'users'
-   id = db.Column(db.Integer, primary_key=True)
-   username = db.Column(db.String(64), unique=True, index=True)
-   role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-   def __repr__ (self):
-      return '<User %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-   form = NameForm()
-   if form.validate_on_submit():
-      user = User.query.filter_by(username = form.name.data).first()
-      if user is None:
-         user = User(username = form.name.data)
-         db.session.add(user)
-         session['known'] = False
-      else:
-         session['known'] = True
-      session['name'] = form.name.data
-      form.name.data = ''
-      return redirect(url_for('index'))
-   return render_template('index.html', form = form, name = session.get('name'), known = session.get('known', False))
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+
 
 @app.route('/user/<name>')
 def user(name):
-   return render_template('user.html', name = name)
+    return render_template('user.html', name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-   return render_template('404.html'), 404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-   return render_template('500.html'), 500
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
