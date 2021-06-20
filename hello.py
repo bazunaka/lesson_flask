50ce8e96afe637c82aaff1c46726463633d21bcb
bazunaka-macbook <bazunaka32@gmail.com>
2021-06-20 14:31:07 +0300

    work

--- hello.py
+++ hello.py
@@ -12,84 +12,31 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class NameForm(Form):
-    name = StringField('Ваше имя?', validators=[Required()])
-    submit = SubmitField('Подтвердить')
-
-
 app = Flask(__name__)
-manager = Manager(app)
-app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:flasflask.db'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
 
 bootstrap = Bootstrap(app)
-db = SQLAlchemy(app)
 
 
-def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
-
-
-manager.add_command("shell", Shell(make_context=make_shell_context))
-
-#migrate = Migrate(app, db)
-#manager.add_command('db', MigrateCommand)
-
-mail = Mail(app)
-
-
-class Role (db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
-class User (db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/index")
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        form.name.data = ''
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+    return render_template("index.html")
 
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route("/user/<name>")
+def user():
+    return render_template("user.html")
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template("500.html"), 500
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip3 install flask
+pip3 install flask_bootstrap
+pip3 install flask_wtf
+pip3 install flask_script
+pip3 install flask_sqlalchemy
+pip3 install wtforms
+pip3 install flask_migrate
+pip3 install flask_mail
